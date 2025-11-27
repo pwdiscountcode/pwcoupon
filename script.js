9913f19967c131007c1126af736a1356d849a928
stanlyjackson96 <stanlyjackson96@gmail.com>
2025-11-27 11:38:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Interactions for PW coupon landing page
-const COUPON = 'MELSEL0001';
+const COUPON = 'KIBSEL0001';
 const PW_URL = 'https://pw.live'; // change to actual PW checkout/course URL if you want
 
 function copyToClipboard(text, btn){
